af69ef4604a6b166387675f82af1f2e1fe67943b
Aek howmoney <gameming2548@gmail.com>
2025-06-19 08:25:20 +0700

    Edit Bug Dump Cookie

--- background.js
+++ background.js
@@ -1,7 +1,5 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === 'buttonClicked') {
-    chrome.cookies.getAll({ domain: "popmartth.rocket-booking.app" }, function(cookies) {
-        console.log("Cookies: ", cookies);
-    });
+    console.log(message.cookies)
   }
 });
--- popup.js
+++ popup.js
@@ -1,3 +1,18 @@
-document.getElementById('btn').addEventListener('click', () => {
-  chrome.runtime.sendMessage({ action: 'buttonClicked' });
-});
+    (async function() {
+        let [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+        chrome.cookies.getAll({"url":tab.url}, cookies => {
+            var area = document.createElement('textarea');
+            area.style.position = 'absolute';
+            area.style.border = '0';
+            area.style.padding = '0';
+            area.style.margin = '0';
+            area.style.height = '1px';
+            area.style.top = '-10px';
+            area.innerText = JSON.stringify(cookies);
+            document.body.appendChild(area, document.body.firstChild);
+            area.focus();
+            area.select();
+            document.execCommand("copy");
+            document.body.removeChild(area);
+        });
+    })();
